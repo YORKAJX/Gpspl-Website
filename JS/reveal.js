a39cd2da443dd558d85dc437edd2a6f1ae92ce7c
Keshav Khurana <s4117077@student.rmit.edu.au>
2025-12-18 10:07:22 +0530

    some changes requires more fixes

---
+++ JS/reveal.js
@@ -0,0 +1,11 @@
+const revealObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.reveal').forEach((el) => {
+  revealObserver.observe(el);
+});
